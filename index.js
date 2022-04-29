700b72075654eaa405e1fd844bcd7440f618147d
Rohit <100916625+rohit-kep@users.noreply.github.com>
2022-04-29 11:13:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var request = new XMLHttpRequest();
+var button = document.getElementById("randomgenrator");
+var random = document.getElementById("random")
+var maincontent = document.getElementById("mainContent");
+
+
+request.open("GET",'https://api.adviceslip.com/advice',true);
+
+request.onload = function(){
+    var data = JSON.parse(this.response);
+
+    if(request.status >=200 &&  request.status <400){
+         let str = random.textContent;
+         random.textContent = str + String(data.slip.id);
+         maincontent.textContent = '"'+data.slip.advice+'"';
+
+    }else{
+        console.log("error");
+    }
+}
+
+
+
+    request.send();
+
